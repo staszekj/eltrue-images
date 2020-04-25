b25b4b5ff9ccac6fb1ccfe82fff959fc5144086d
Jagiella, Stanislaw <stanislaw.jagiella.ctr@sabre.com>
2020-04-25 21:44:33 +0200

    Add router

--- src/client/redux/root-reducer.tsx
+++ src/client/redux/root-reducer.tsx
@@ -1,25 +1,31 @@
-import {combineReducers} from "redux";
+import { combineReducers, Reducer } from "redux";
 
-import {searchEndpointReducer, TSearchEndpoint} from "./search-endpoint-reducer"
-import {searchComponentReducer, TSearchCompomentReducer} from "./search-component-reducer"
+import { History } from "history";
 
-import {oneImageComponentReducer, TOneImageComponentReducer} from "./one-image-component-reducer";
-import {authorUpdateEndpointReducer, TAuthorUpdateEndpoint} from "./author-update-endpoint-reducer";
-import {themeReducer, TThemeReducer} from "./theme-reducer";
+
+import { searchEndpointReducer, TSearchEndpoint } from "./search-endpoint-reducer";
+import { searchComponentReducer, TSearchCompomentReducer } from "./search-component-reducer";
+
+import { oneImageComponentReducer, TOneImageComponentReducer } from "./one-image-component-reducer";
+import { authorUpdateEndpointReducer, TAuthorUpdateEndpoint } from "./author-update-endpoint-reducer";
+import { themeReducer, TThemeReducer } from "./theme-reducer";
+import { connectRouter, RouterState } from "connected-react-router";
 
 export interface TRootState {
-    searchEndpoint: TSearchEndpoint,
-    authorUpdateEndpoint: TAuthorUpdateEndpoint,
-    searchComponent: TSearchCompomentReducer,
-    oneImageComponent: TOneImageComponentReducer,
-    theme: TThemeReducer
+  searchEndpoint: TSearchEndpoint,
+  authorUpdateEndpoint: TAuthorUpdateEndpoint,
+  searchComponent: TSearchCompomentReducer,
+  oneImageComponent: TOneImageComponentReducer,
+  theme: TThemeReducer,
+  router: RouterState
 }
 
-export const rootReducer = combineReducers<TRootState>({
-    searchEndpoint: searchEndpointReducer,
-    authorUpdateEndpoint: authorUpdateEndpointReducer,
-    searchComponent: searchComponentReducer,
-    oneImageComponent: oneImageComponentReducer,
-    theme: themeReducer
+export const rootReducer = (history: History) => combineReducers<TRootState>({
+  searchEndpoint: searchEndpointReducer,
+  authorUpdateEndpoint: authorUpdateEndpointReducer,
+  searchComponent: searchComponentReducer,
+  oneImageComponent: oneImageComponentReducer,
+  theme: themeReducer,
+  router: connectRouter(history)
 });
 
